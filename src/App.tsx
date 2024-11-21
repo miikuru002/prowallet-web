0fb5e1065ba78f6498b1df0f8259cc49e474ff4a
J. Ortega <jamutaqortegavelez@gmail.com>
2024-11-21 17:40:10 -0500

    feat: add Spanish locale configuration for PrimeReact date and time components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { LayoutProvider } from "./layout/context/layoutcontext";
-import { PrimeReactProvider } from "primereact/api";
+import { addLocale, PrimeReactProvider } from "primereact/api";
 import "primereact/resources/primereact.css";
 import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
@@ -15,6 +15,17 @@ import { userManager } from "./config/authConfig";
 function App() {
 	const Routes = useRoutes(MainRoutes);
 	const queryClient = new QueryClient();
+
+	addLocale('es', {
+		firstDayOfWeek: 1,
+		dayNames: ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'],
+		dayNamesShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+		dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
+		monthNames: ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
+		monthNamesShort: ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'],
+		today: 'Hoy',
+		clear: 'Limpiar'
+	});
 
 	return (
 		<AuthProvider
